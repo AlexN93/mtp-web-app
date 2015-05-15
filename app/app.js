3b47012a1887ff5f5c7a6b6fa8c06a5b790e30cd
AlexN <aleksandar.m.nikolov@gmail.com>
2015-05-15 15:53:49 +0300

    some factory changes

--- app/app.js
+++ app/app.js
@@ -32,76 +32,72 @@ angular.module('mtpApp', [
         }
     };
 }])
-.factory('DataService', ["$filter",
-    function($filter) {
-        var getHttpLogic = function($http, $url) {
-            return $http.get($url);
-        };
-        var formatDataLogic = function(data) {
-            var dataArray = [['Countries', 'Transaction count']];
-            for(var i=0; i < data.length; i++) {
-                dataArray.push([data[i]["TransactionOrigin"], parseInt(data[i]["TransactionCount"])]);
-            }
-            return dataArray;
-        };
-        var formatTableDataLogic = function(data) {
-            var dataArray = [];
-            for(var i=0; i < data.length; i++) {
-                var array = [
-                    parseInt(data[i]["TransactionUserID"]),
-                    data[i]["TransactionCurrencyFrom"],
-                    data[i]["TransactionCurrencyTo"],
-                    parseFloat(data[i]["TransactionAmountSell"]),
-                    parseFloat(data[i]["TransactionAmountBuy"]),
-                    parseFloat(data[i]["TransactionRate"]),
-                    data[i]["TransactionTime"]["date"],
-                    data[i]["TransactionOrigin"]
-                ];
-                dataArray.push(array);
-            }
-            return dataArray;
-        };
-        return {
-            getHttp: getHttpLogic,
-            formatData: formatDataLogic,
-            formatTableData: formatTableDataLogic
-        };
-    }
-])
-.factory('SocketService', ["$filter",
-    function($filter) {
-        var addDataLogic = function(transaction, data) {
-            var flag = 0;
-            for(var i=0; i < data.length; i++) {
-                if(transaction["originatingCountry"] == data[i][0]){
-                    data[i][1] = parseInt(data[i][1]) + 1;
-                    flag = 1;
-                }
-            }
-            if(flag == 0) {
-                data.push([transaction["originatingCountry"], 1]);
-            }
-            return data;
-        };
-        var addTableDataLogic = function(transaction) {
+.factory('DataService', function() {
+    var getHttp = function($http, $url) {
+        return $http.get($url);
+    };
+    var formatData = function(data) {
+        var dataArray = [['Countries', 'Transaction count']];
+        for(var i=0; i < data.length; i++) {
+            dataArray.push([data[i]["TransactionOrigin"], parseInt(data[i]["TransactionCount"])]);
+        }
+        return dataArray;
+    };
+    var formatTableData = function(data) {
+        var dataArray = [];
+        for(var i=0; i < data.length; i++) {
             var array = [
-                parseInt(transaction["userId"]),
-                transaction["currencyFrom"],
-                transaction["currencyTo"],
-                parseFloat(transaction["amountSell"]),
-                parseFloat(transaction["amountBuy"]),
-                parseFloat(transaction["rate"]),
-                transaction["timePlaced"],
-                transaction["originatingCountry"]
+                parseInt(data[i]["TransactionUserID"]),
+                data[i]["TransactionCurrencyFrom"],
+                data[i]["TransactionCurrencyTo"],
+                parseFloat(data[i]["TransactionAmountSell"]),
+                parseFloat(data[i]["TransactionAmountBuy"]),
+                parseFloat(data[i]["TransactionRate"]),
+                data[i]["TransactionTime"]["date"],
+                data[i]["TransactionOrigin"]
             ];
-            return array;
-        };
-        return {
-            addData: addDataLogic,
-            addTableData: addTableDataLogic
-        };
-    }
-])
+            dataArray.push(array);
+        }
+        return dataArray;
+    };
+    return {
+        getHttp: getHttp,
+        formatData: formatData,
+        formatTableData: formatTableData
+    };
+})
+.factory('SocketService', function() {
+    var addData = function(transaction, data) {
+        var flag = 0;
+        for(var i=0; i < data.length; i++) {
+            if(transaction["originatingCountry"] == data[i][0]){
+                data[i][1] = parseInt(data[i][1]) + 1;
+                flag = 1;
+            }
+        }
+        if(flag == 0) {
+            data.push([transaction["originatingCountry"], 1]);
+        }
+        return data;
+    };
+    var addTableData = function(transaction) {
+        var array = [
+            parseInt(transaction["userId"]),
+            transaction["currencyFrom"],
+            transaction["currencyTo"],
+            parseFloat(transaction["amountSell"]),
+            parseFloat(transaction["amountBuy"]),
+            parseFloat(transaction["rate"]),
+            transaction["timePlaced"],
+            transaction["originatingCountry"]
+        ];
+        return array;
+    };
+    return {
+        addData: addData,
+        addTableData: addTableData
+    };
+})
 .factory('ActiveController', function () {
     var activeController;
     function setActiveController(name) {
@@ -116,7 +112,7 @@ angular.module('mtpApp', [
     }
 })
 .factory('HostService', function () {
-    var apiHost = "https://mtp-api.herokuapp.com";
+    var apiHost = "http://mtpapi.local.com/app_dev.php";
     function getApiHost() {
         return apiHost;
     }
